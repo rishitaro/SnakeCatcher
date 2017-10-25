25d46f1d1cf59139cc4c643792af81bc05e0eb17
rishitaro <riroy@ucsc.edu>
2017-10-24 23:37:03 -0700

    Added Splash activity

---
+++ SnakeCatcher/app/src/main/java/pythonsrus/snakecatcher/Splash.java
@@ -0,0 +1,24 @@
+package pythonsrus.snakecatcher;
+
+import android.os.Handler;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        int secondsDelayed = 3;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splash.this, Main2Activity.class));
+                finish();
+            }
+        }, secondsDelayed *1000);
+    }
+}
+
